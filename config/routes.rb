7f4380d78405d11907c80dad6f007bd5f3ae45d4
vector800 <blacklotussumaho@gmail.com>
2025-04-04 11:07:10 +0900

    Finish page-layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root 'top_pages#top'
+  get '/new_tweet', to: 'tweets#new', as: 'new_tweet'
 end
---
+++ test/controllers/tweets_controller_test.rb
@@ -0,0 +1,8 @@
+require "test_helper"
+
+class TweetsControllerTest < ActionDispatch::IntegrationTest
+  test "should get new" do
+    get tweets_new_url
+    assert_response :success
+  end
+end
